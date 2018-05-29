81433f0cdae2354e46a3272af0f1e06fb11c4683
Praveen Kumar Pandey <praveenkpandey@gmail.com>
2018-05-29 16:55:23 +0530

    First Commit

---
+++ app/src/main/java/com/example/praveenpandey/workoutsadvisor/find_workout.java
@@ -0,0 +1,13 @@
+package com.example.praveenpandey.workoutsadvisor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class find_workout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_workout);
+    }
+}
